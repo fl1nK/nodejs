7668f990bad9029413c52aec103dcefeaff696e2
= <=>
2022-10-24 20:17:17 +0300

    add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,14 @@
 module.exports = {
-  env: {
-    es2021: true,
-    node: true
-  },
-  extends: 'standard',
-  overrides: [
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  },
-  rules: {
-  }
+    env: {
+        es2021: true,
+        node: true,
+    },
+    extends: ['standard', 'plugin:sonarjs/recommended'],
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['sonarjs'],
+    rules: {},
 }
