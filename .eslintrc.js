d425c6af207b7261a8935ebd0b828e6ecb6732c1
= <=>
2022-10-24 20:33:19 +0300

    instal eslint-config-prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,13 @@
 module.exports = {
     env: {
         es2021: true,
-        node: true,
+        node: true
     },
-    extends: ['standard', 'plugin:sonarjs/recommended'],
+    extends: ['standard', 'plugin:sonarjs/recommended', 'prettier'],
     overrides: [],
     parserOptions: {
         ecmaVersion: 'latest',
-        sourceType: 'module',
+        sourceType: 'module'
     },
     plugins: ['sonarjs'],
     rules: {},
