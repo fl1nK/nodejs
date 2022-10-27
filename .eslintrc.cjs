af9296be915067bd9c484eb23a826cfad40a5fd6
= <=>
2022-10-27 16:06:33 +0300

    fix .prettierrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,19 @@
 module.exports = {
-    env: {
-        es2021: true,
-        node: true,
-    },
-    extends: [
-        'standard',
-        'plugin:n/recommended',
-        'plugin:sonarjs/recommended',
-        'prettier',
-    ],
-    overrides: [],
-    parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-    },
-    plugins: ['sonarjs', 'n'],
-    rules: {},
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'standard',
+    'plugin:n/recommended',
+    'plugin:sonarjs/recommended',
+    'prettier',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['sonarjs', 'n'],
+  rules: {},
 }
